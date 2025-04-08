ab65c14dbb1568d32a03287f17b76ece7638a633
Chameleeon <danichelsea3@gmail.com>
2025-04-08 15:58:42 +0200

    Connect to cs2.exe. Execute bunnyhop while space is pressed

--- kernel_mode/src/main.cpp
+++ kernel_mode/src/main.cpp
@@ -1,31 +1,32 @@
 #include <ntifs.h>
-
 extern "C" {
 	NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName, PDRIVER_INITIALIZE InitializationFunction);
-
 	NTKERNELAPI NTSTATUS MmCopyVirtualMemory(PEPROCESS SourceProcess, PVOID SourceAddress, PEPROCESS TargetProcess, PVOID TargetAddress, SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode, PSIZE_T ReturnSize);
 }
 
 void debug_print(PCSTR text) {
-
 #ifndef DEBUG
 	UNREFERENCED_PARAMETER(text);
-#endif // DEBUG
+#endif
 
 	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, text));
 }
 
 namespace driver {
 	namespace codes {
-		// Attack to process
-		constexpr ULONG attach = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
-		// Read process memory
-		constexpr ULONG read = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
-		// Write process memory
-		constexpr ULONG write = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		// Attach to process
+		constexpr ULONG attach =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		
+		// read process memory
+		constexpr ULONG read =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		
+		// write to process memory
+		constexpr ULONG write =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
 	}
 
-	// Shared between user mode & kernel mode
 	struct Request {
 		HANDLE process_id;
 
@@ -36,7 +37,7 @@ namespace driver {
 		SIZE_T return_size;
 	};
 
-	NTSTATUS create(PDEVICE_OBJECT device_object, PIRP irp){
+	NTSTATUS create(PDEVICE_OBJECT device_object, PIRP irp) {
 		UNREFERENCED_PARAMETER(device_object);
 
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -52,7 +53,7 @@ namespace driver {
 		return irp->IoStatus.Status;
 	}
 
-	// TODO
+
 	NTSTATUS device_control(PDEVICE_OBJECT device_object, PIRP irp) {
 		UNREFERENCED_PARAMETER(device_object);
 
@@ -60,10 +61,8 @@ namespace driver {
 
 		NTSTATUS status = STATUS_UNSUCCESSFUL;
 
-		//Determine which code was passed through.
 		PIO_STACK_LOCATION stack_irp = IoGetCurrentIrpStackLocation(irp);
 
-		// Access the request object sent from user mode.
 		auto request = reinterpret_cast<Request*>(irp->AssociatedIrp.SystemBuffer);
 
 		if (stack_irp == nullptr || request == nullptr) {
@@ -75,25 +74,22 @@ namespace driver {
 
 		const ULONG control_code = stack_irp->Parameters.DeviceIoControl.IoControlCode;
 		switch (control_code) {
-			case codes::attach:
-				// Attach to the target process
-				status = PsLookupProcessByProcessId(request->process_id, &target_process);
-				break;
+		case codes::attach:
+			status = PsLookupProcessByProcessId(request->process_id, &target_process);
+			break;
+		case codes::read:
+			if (target_process != nullptr) {
+				status = MmCopyVirtualMemory(target_process, request->target, PsGetCurrentProcess(), request->buffer, request->size, KernelMode, &request->return_size);
+			}
+			break;
+		case codes::write:
+			if (target_process != nullptr) {
+				status = MmCopyVirtualMemory(PsGetCurrentProcess(), request->buffer, target_process, request->target, request->size, KernelMode, &request->return_size);
+			}
+			break;
 
-			case codes::read:
-				if (target_process != nullptr) {
-					// Copy memory from target process
-					status = MmCopyVirtualMemory(target_process, request->target, PsGetCurrentProcess(), request->buffer, request->size, KernelMode, &request->return_size);
-				}
-				break;
-			case codes::write:
-				if (target_process != nullptr) {
-					// Copy memory into target process
-					status = MmCopyVirtualMemory(PsGetCurrentProcess(), request->buffer, target_process, request->target, request->size, KernelMode, &request->return_size);
-				}
-				break;
-			default:
-				break;
+		default:
+			break;
 		}
 
 		irp->IoStatus.Status = status;
@@ -101,58 +97,57 @@ namespace driver {
 
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
 
-		return status;
+		return irp->IoStatus.Status;
 	}
-
 }
 
 NTSTATUS driver_main(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
 	UNREFERENCED_PARAMETER(registry_path);
 
 	UNICODE_STRING device_name = {};
-	RtlInitUnicodeString(&device_name, L"\\Device\\BunnyDriver");
+	RtlInitUnicodeString(&device_name, L"\\Device\\ArchyDriver");
 
-	// Create driver device object
 	PDEVICE_OBJECT device_object = nullptr;
 	NTSTATUS status = IoCreateDevice(driver_object, 0, &device_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
 
 	if (status != STATUS_SUCCESS) {
-		debug_print("[-] Failed to create driver device.\n");
+		debug_print("[-] Failed to create driver evice.\n");
 		return status;
 	}
 
 	debug_print("[+] Driver device successfully created.\n");
 
 	UNICODE_STRING symbolic_link = {};
-	RtlInitUnicodeString(&symbolic_link, L"\\DosDevices\\BunnyDriver");
+	RtlInitUnicodeString(&symbolic_link, L"\\DosDevices\\ArchyDriver");
 
 	status = IoCreateSymbolicLink(&symbolic_link, &device_name);
-	if (status != STATUS_SUCCESS) {
+	if (status != STATUS_SUCCESS)
+	{
 		debug_print("[-] Failed to establish symbolic link.\n");
 		return status;
 	}
 
-	debug_print("[+] Driver symbolic link successfully established.\n");
+	debug_print("[+] Driver symbolic link successfully created.\n");
 
-	// Allow sending small amounts of data between user mode and kernel mode
+	// enable sending small amounts of data between usermode and kernelmode
 	SetFlag(device_object->Flags, DO_BUFFERED_IO);
 
 	driver_object->MajorFunction[IRP_MJ_CREATE] = driver::create;
 	driver_object->MajorFunction[IRP_MJ_CLOSE] = driver::close;
 	driver_object->MajorFunction[IRP_MJ_DEVICE_CONTROL] = driver::device_control;
-
+	
 	ClearFlag(device_object->Flags, DO_DEVICE_INITIALIZING);
 
-	debug_print("[+] Driver initialized successfully.\n");
+	debug_print("[+] Driver Initialized Successfully.\n");
 
 	return status;
 }
 
 NTSTATUS DriverEntry() {
-	debug_print("[+] Hello World from the kernel!\n");
+	debug_print("[+] Hello from kernel!\n");
 
 	UNICODE_STRING driver_name = {};
-	RtlInitUnicodeString(&driver_name, L"\\Driver\\BunnyDriver");
-
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\ArchyDriver");
+	
 	return IoCreateDriver(&driver_name, &driver_main);
 }
